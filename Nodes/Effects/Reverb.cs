d894fe77c2e43323ab1871e0bc88271fa9825550
kevinbyrom <kevinbyrom@gmail.com>
2024-04-30 11:37:47 -0700

    Tweaks to reverb

--- Nodes/Effects/Reverb.cs
+++ Nodes/Effects/Reverb.cs
@@ -43,16 +43,16 @@ namespace Composer.Nodes.Effects
 
             Signal signal = this.Input.Signal;
 
+            double val = 0;
+
             for (int i = 0; i < this.delayNodes.Length; i++)
             {
                 this.delayNodes[i].Update(time);
 
-                var delaySignal = this.delayNodes[i].Signal * ((i + 1) * 0.5);
-
-                signal += delaySignal;
+                val += this.delayNodes[i].Signal.Value;
             }
 
-            this.Signal = signal;
+            this.Signal = new Signal(val / NumDelayNodes);
         }
     }
 
