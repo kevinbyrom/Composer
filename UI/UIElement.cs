b818d798369067a4850d6dedf95f37855514bfaf
kevinbyrom <kevinbyrom@gmail.com>
2024-04-30 19:02:46 -0700

    Bringing in a UIManager to handle UI elements

---
+++ UI/UIElement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Composer.UI
+{
+   /* internal class UIElement 
+    {
+        public UIManager UI { get; private set; }
+
+        public Vector2 ScreenPos;
+
+        public Vector2 Size;
+
+        protected List<UIElement> subElements = new List<UIElement>();
+
+        protected RenderTarget2D renderTarget;
+
+        protected Texture2D texture;
+
+        public Color Color { get; set; }
+
+        public UIElement(UIManager ui, int x, int y, int width, int height) 
+        {
+            this.UI = ui;
+            this.ScreenPos.X = x;
+            this.ScreenPos.Y = y;
+            this.Size.X = width;
+            this.Size.Y = height;
+            this.Color = Color.Transparent;
+            this.renderTarget = new RenderTarget2D(this.UI.Game.GraphicsDevice, texture.Width, texture.Height);
+        }
+
+        public UIElement(UIManager ui, int x, int y, int width, int height, Color color) : this(UIManager ui, x, y, width, height)
+        {
+            this.Color = color;
+        }
+
+        public void AddSubElement(UIElement element)
+        {
+            subElements.Add(element);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch, double time)
+        {
+            this.UI.PushRenderTarget(this.renderTarget);
+            this.UI.Clear(this.Color);
+
+
+            // Draw to the render target
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, this.ScreenPos, Color.White);
+            foreach (var subElement in subElements)
+            {
+                subElement.Draw(spriteBatch, time);
+            }
+            spriteBatch.End();
+
+            this.UI.Game.GraphicsDevice.SetRenderTarget(null);
+
+            // Draw the render target to the screen
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            spriteBatch.Draw(renderTarget, this.ScreenPos, Color.White);
+            spriteBatch.End();
+
+        }
+    }*/
+}
---
+++ UI/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Composer.UI
+{
+    public class UIManager
+    {
+        public Game Game { get; private set; }
+        private Stack<RenderTarget2D> renderTargets = new Stack<RenderTarget2D>();
+        private RenderTarget2D currRenderTarget = null;
+
+        public UIManager(Game game) 
+        {
+            this.Game = game;
+        }
+
+        public void PushRenderTarget(RenderTarget2D renderTarget)
+        {
+            this.Game.GraphicsDevice.SetRenderTarget(renderTarget);
+
+            if (this.currRenderTarget == null)
+                this.renderTargets.Push(this.currRenderTarget);
+
+            this.currRenderTarget= renderTarget;
+        }
+
+        public void PopRenderTarget()
+        {
+            this.Game.GraphicsDevice.SetRenderTarget(this.renderTargets.Pop());
+        }
+
+        public void ClearRenderTargets()
+        { 
+            this.renderTargets.Clear();
+            this.currRenderTarget = null;
+            this.Game.GraphicsDevice.SetRenderTarget(null);
+        }
+
+
+        public void Clear(Color color)
+        {
+            this.Game.GraphicsDevice.Clear(color);
+        }
+    }
+}
